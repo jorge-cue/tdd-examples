a31ea17f49c7ad46670076f6f2fb606bec0c7ed2
Jorge H. Cue Cantu <jorge.cue@gmail>
2021-11-23 11:13:36 -0600

    Initial commit

---
+++ src/main/java/com/example/tddexamples/TddExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tddexamples/TddExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
